72ad657cefe3c590a8873ce616b17788213d9d3c
ShenJX21 <101643317+ShenJX21@users.noreply.github.com>
2023-06-09 10:26:53 +0800

    Update craw_data.py

--- code/craw_data.py
+++ code/craw_data.py
@@ -21,8 +21,8 @@ from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 
 def main():
     start_date = date(2023, 5, 1)
-    end_date = datetime.now().date()
-    #end_date = datetime.date.today()
+    end_date = date(2023, 6, 1)
+    #end_date = datetime.now().date()
     craw_data(start_date, end_date)
 
 
